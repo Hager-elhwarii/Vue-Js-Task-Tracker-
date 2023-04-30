fba410adbfc99cda97649ac273aff090f0db65e7
hager-dot <hagerelhwarii6633@gmail.com>
2023-04-30 07:36:52 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <h1>Task Tracker</h1>
+    </header>
+</template>
+
+
+<script>
+export default {
+name : "Header",
+}
+</script>
+
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// this is the entery point of Vue 
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+// importing the root app component
+import App from "./App.vue";
+
+createApp(App).mount("#app");
